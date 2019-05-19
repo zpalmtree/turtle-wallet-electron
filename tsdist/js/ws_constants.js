ac1702af2fc5ac8062367368565d9d3b67c6dfec
ExtraHash <ExtraHash@protonmail.com>
2019-05-19 12:08:41 -0600

    don't commit the compiled javascript

--- tsdist/js/ws_constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.freeze({
-    syncStatus: {
-        IDLE: -100,
-        RESET: -300,
-        NODE_ERROR: -200,
-        NET_ONLINE: -10,
-        NET_OFFLINE: -50
-    }
-});
